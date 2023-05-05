465c511ca6d66569181f274e67b5d4d7a3d3dd7a
helal <helal@localhost.com>
2023-05-05 22:23:29 +0600

    2nd build of nestjs-app

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is my first NestJS project !';
+    return 'This is my first NestJS project, 2nd build !';
   }
 }
